ab72a1dc0dda09bfcd21e405323b97db3a54d594
Eduardo Paludo <eduardo.paludo8@gmail.com>
2023-06-09 20:23:14 -0300

    javascript added

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,13 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+$(document).ready(function() {
+    // Attach event listener to the delete button
+    $(".delete_button form").on("submit", function(e) {
+    // Display the confirmation popup
+    if (!confirm("Are you sure you want to delete this entry?")) {
+        e.preventDefault(); // Cancel form submission if user cancels
+    }
+    });
+});
